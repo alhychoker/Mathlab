30ea18b7c7c3c61926cff2e58f125cf330462416
CHOKER <89501742+alhychoker@users.noreply.github.com>
2021-08-25 23:10:40 +0700

    Update TUGAS2NO3.m

--- TUGAS2NO3.m
+++ TUGAS2NO3.m
@@ -1,3 +1,27 @@
  size(vektor_1), size(vektor_2), size(matriks)
   prod(size(vektor_1)), ... 
 prod(size(vektor_2)), prod(size(matriks))
+
+ans =
+
+     3     1
+
+
+ans =
+
+     4     4
+
+
+ans =
+
+     4
+
+
+ans =
+
+     3
+
+
+ans =
+
+    16
