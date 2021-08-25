bfede4aeb78b77ce9b6e24369856c8efeab7dd30
CHOKER <89501742+alhychoker@users.noreply.github.com>
2021-08-25 22:43:10 +0700

    Delete TUGAS3NO5.m

--- TUGAS3NO5.m
+++
@@ -1,7 +0,0 @@
-x = -5:0.1:5; % membuat vektor x 
-sinus=sinh(x); cosinus=cosh(x); tangent=tanh(x);
-clc % membersihkan layar 
-disp("Tabel hiperbolik-trigonometri:")
-disp("x sinh cosh tanh"), 
-disp('--------------------------------') 
-['x sinus cosinus tangent']
